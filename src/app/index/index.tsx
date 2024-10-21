192b8e5a1e6e490ec6720491750ad86ab2c0ac46
Samuel Oliveira <tcguedesss@outlook.com>
2024-10-21 16:05:38 -0300

    segundo commit

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,25 @@
-import { Text, View, StyleSheet } from "react-native"
+import { View, Image, TouchableOpacity } from "react-native"
+
+import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+
+import { Category } from "@/components/category"
+
 
 export default function Index(){
     return(
         <View style={styles.container}>
-            <Text style={styles.title}>Hello React Native!</Text>
-            <Text style={styles.title}>Meu App</Text>
+            <View style={styles.header}>
+                <Image source={require ("@/assets/logo.png")} style={styles.logo} />
+
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
+
+            <Category/>
         </View>
     )
 }
